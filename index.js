a27e3d4c7ba6c647222fa4817025383a060faaae
igntl <76429465+igntl@users.noreply.github.com>
2026-04-08 10:49:06 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+import { Client, GatewayIntentBits, EmbedBuilder } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.on('ready', () => {
+  console.log(`🤖 ${client.user.tag} شغال`);
+});
+
+client.on('guildMemberAdd', async (member) => {
+
+  const channel = member.guild.channels.cache.get("1483219896069525665");
+
+  if (!channel) return;
+
+  const embed = new EmbedBuilder()
+    .setColor("#ffffff")
+    .setDescription(`** Welcome to server TITANIUM 
+
+${member}
+
+  نورت السيرفر وشرفتنا بانضمامك
+.هنا نعيش جو من المتعة، التفاعل، والإبداع والكل مرحّب فيه
+.تأكد إنك تطّلع على القوانين أول، واحرص تكون مشاركتك إيجابية ومحترمة 🤍`)
+    .setImage("https://files.oaiusercontent.com/file-8322455D-9D63-4FF9-AAB3-87C1D039F34B.png");
+
+  const role = member.guild.roles.cache.get("1483258920653947093");
+  if (role) {
+    member.roles.add(role).catch(() => {});
+  }
+
+  channel.send({
+    embeds: [embed]
+  });
+
+});
+
+client.login(process.env.TOKEN);
